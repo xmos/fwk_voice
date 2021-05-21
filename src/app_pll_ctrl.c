380a45fbe9dcc8dc25b152434ba6f57cd0dfe14d
Michael Bruno <mikeb@xmos.com>
2021-05-21 11:51:14 -0400

    Cleanup appPLL rate PID controller
    It makes much more sense now..

--- src/app_pll_ctrl.c
+++ src/app_pll_ctrl.c
@@ -6,50 +6,6 @@
 #include <xcore/hwtimer.h>
 #include "app_pll_ctrl.h"
 
-void app_pll_nudge(int nudge)
-{
-    unsigned tileid = get_local_tile_id();
-
-#if APP_PLL_NUDGE_METHOD == 1
-    /*
-     * This is similar to how 3510/3610 nudges the clock frequency.
-     * Only one write of the slower/faster frequency though doesn't seem
-     * to be very reliable. Doing it 3 times seems better. But this is not
-     * scientific. I think method 2 is safer.
-     */
-    if (nudge > 0) {
-        write_sswitch_reg(tileid, XS1_SSWITCH_SS_APP_PLL_FRAC_N_DIVIDER_NUM, APP_PLL_FRAC_FAST);
-        write_sswitch_reg(tileid, XS1_SSWITCH_SS_APP_PLL_FRAC_N_DIVIDER_NUM, APP_PLL_FRAC_FAST);
-        write_sswitch_reg(tileid, XS1_SSWITCH_SS_APP_PLL_FRAC_N_DIVIDER_NUM, APP_PLL_FRAC_FAST);
-    } else if (nudge < 0) {
-        write_sswitch_reg(tileid, XS1_SSWITCH_SS_APP_PLL_FRAC_N_DIVIDER_NUM, APP_PLL_FRAC_SLOW);
-        write_sswitch_reg(tileid, XS1_SSWITCH_SS_APP_PLL_FRAC_N_DIVIDER_NUM, APP_PLL_FRAC_SLOW);
-        write_sswitch_reg(tileid, XS1_SSWITCH_SS_APP_PLL_FRAC_N_DIVIDER_NUM, APP_PLL_FRAC_SLOW);
-    }
-    write_sswitch_reg_no_ack(tileid, XS1_SSWITCH_SS_APP_PLL_FRAC_N_DIVIDER_NUM, APP_PLL_FRAC_NOM);
-
-#elif APP_PLL_NUDGE_METHOD == 2
-    /* This actually increments or decrements the frequency by a small step */
-    static uint32_t f = (APP_PLL_FRAC_NOM & 0x0000FF00) >> 8;
-    uint32_t fracval = APP_PLL_FRAC_NOM & 0xFFFF00FF;
-
-    if (nudge > 0) {
-        if (f < 255) {
-            f++;
-        }
-    } else if (nudge < 0) {
-        if (f > 0) {
-            f--;
-        }
-    }
-
-    fracval |= (f << 8);
-    write_sswitch_reg_no_ack(tileid, XS1_SSWITCH_SS_APP_PLL_FRAC_N_DIVIDER_NUM, fracval);
-
-//    rtos_printf("%d\n", f);
-#endif
-}
-
 void app_pll_set_numerator(int numerator)
 {
     const unsigned tileid = get_local_tile_id();
--- src/app_pll_ctrl.h
+++ src/app_pll_ctrl.h
@@ -6,30 +6,14 @@
 
 #include "board_init.h" // for AUDIO_CLOCK_FREQUENCY
 
-#ifndef APP_PLL_NUDGE_METHOD
-#define APP_PLL_NUDGE_METHOD 2
-#endif
 
 #if (AUDIO_CLOCK_FREQUENCY != 24576000)
 #error PLL values only valid if AUDIO_CLOCK_FREQUENCY == 24576000
 #endif
 
-#define APP_PLL_CTL_VAL 0x0A019803 // Valid for all fractional values
+#define APP_PLL_CTL_VAL   0x0A019803 // Valid for all fractional values
+#define APP_PLL_FRAC_NOM  0x800095F9 // 24.576000 MHz
 
-#if APP_PLL_NUDGE_METHOD == 1
-#define APP_PLL_FRAC_NOM   0x80000204 // 24576000
-#define APP_PLL_FRAC_SLOW  0x80000209 // 24558000 ~0.07% slower
-#define APP_PLL_FRAC_FAST  0x80000809 // 24594000 ~0.07% faster
-#elif APP_PLL_NUDGE_METHOD == 2
-#define APP_PLL_FRAC_NOM   0x800095F9 // 24576000
-#define APP_PLL_FRAC_SLOW  0x80004AF9 // 24558000 ~0.07% slower
-#define APP_PLL_FRAC_FAST  0x8000E0F9 // 24594000 ~0.07% faster
-#endif
-
-#define APP_PLL_NUDGE_FASTER  1
-#define APP_PLL_NUDGE_SLOWER -1
-
-void app_pll_nudge(int nudge);
 void app_pll_set_numerator(int numerator);
 void app_pll_init(void);
 
