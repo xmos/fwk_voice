7685566fa4affbe7deaf7ec192df1c63205b57f3
Michael Bruno <mikeb@xmos.com>
2021-06-08 20:47:34 -0400

    Simplify device control servicer registration
    Also remove some new clang warnings

--- src/app_control/app_control.h
+++ src/app_control/app_control.h
@@ -8,12 +8,12 @@
 
 #include "app_conf.h"
 
-#define APP_CONTROL_TRANSPORT_COUNT ((appconfI2C_CTRL_ENABLED ? 1 : 0) + (appconfUSB_ENABLED ? 1 : 0))
-
-extern device_control_t *device_control_ctxs[APP_CONTROL_TRANSPORT_COUNT];
 extern device_control_t *device_control_i2c_ctx;
 extern device_control_t *device_control_usb_ctx;
 
+control_ret_t app_control_servicer_register(device_control_servicer_t *ctx,
+                                            const control_resid_t resources[],
+                                            size_t num_resources);
 int app_control_init(void);
 
 #endif /* APP_CONTROL_H_ */
