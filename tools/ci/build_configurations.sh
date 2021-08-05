761852dc074a135ada3f099690fbe2bff32ef806
Keith Moulton <keithm@xmos.com>
2021-08-05 14:54:31 -0400

    fixes

--- tools/ci/build_configurations.sh
+++ tools/ci/build_configurations.sh
@@ -5,15 +5,20 @@ DISTDIR=dist
 
 mkdir -p ${DISTDIR}
 
+#NOTE: The defines need to be separated by a colon and not whitespace
+export APP_CONF_DEFINES="-DappconfI2S_ENABLED=1;-DappconfUSB_ENABLED=1;-DappconfUSB_AUDIO_MODE=0"
+
 echo '******************************************************'
 echo '* Building UA configuration'
 echo '******************************************************'
-(make distclean all WW=0 CMAKE_ARGS="-DappconfI2S_ENABLED=1 -DappconfUSB_ENABLED=1 -DappconfUSB_AUDIO_MODE=0")
+(make distclean all WW=0)
 (cp bin/sw_xvf3652.xe ${DISTDIR}/sw_xvf3652_UA.xe)
 
+#NOTE: The defines need to be separated by a colon and not whitespace
+export APP_CONF_DEFINES="-DappconfI2S_ENABLED=1;-DappconfUSB_ENABLED=0"
 
 echo '******************************************************'
 echo '* Building INT configuration'
 echo '******************************************************'
-(make distclean all WW=0 CMAKE_ARGS="-DappconfI2S_ENABLED=1 -DappconfUSB_ENABLED=0")
+(make distclean all WW=0)
 (cp bin/sw_xvf3652.xe ${DISTDIR}/sw_xvf3652_INT.xe)
