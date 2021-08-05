fe3e45f0b86eacf1dd769aea9d413cff6dc4d7dc
Keith Moulton <keithm@xmos.com>
2021-08-05 11:10:17 -0400

    added more debugging

--- tools/ci/build_configurations.sh
+++ tools/ci/build_configurations.sh
@@ -8,12 +8,12 @@ mkdir -p ${DISTDIR}
 echo '******************************************************'
 echo '* Building UA configuration'
 echo '******************************************************'
-(make distclean all WW=0 appconfI2S_ENABLED=1 appconfUSB_ENABLED=1)
+(make distclean all WW=0 CMAKE_ARGS="-DappconfI2S_ENABLED=1 -DappconfUSB_ENABLED=1 -DappconfUSB_AUDIO_MODE=0 -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON")
 (cp bin/sw_xvf3652.xe ${DISTDIR}/sw_xvf3652_UA.xe)
 
 
 echo '******************************************************'
 echo '* Building INT configuration'
 echo '******************************************************'
-(make distclean all WW=0 appconfI2S_ENABLED=1 appconfUSB_ENABLED=0 appconfUSB_AUDIO_MODE=0)
+(make distclean all WW=0 CMAKE_ARGS="-DappconfI2S_ENABLED=1 -DappconfUSB_ENABLED=0 -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON")
 (cp bin/sw_xvf3652.xe ${DISTDIR}/sw_xvf3652_INT.xe)
