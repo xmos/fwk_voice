202c408d5f6ce1c2432b42a6888a4aca1df7931e
Keith Moulton <keithm@xmos.com>
2021-10-26 09:43:39 -0400

    updated to python 3, fixed indentation, added install and run instructions

--- test/test_wakeword_detection.py
+++ test/test_wakeword_detection.py
@@ -19,19 +19,19 @@ success_count = 0
 
 # Iterate over the log file to tally tests and successes
 with open(default_log_file, "r") as f:
-	for line in f:
-		for match in re.finditer(regex_test, line, re.S):
-			# print line.replace(regex_test, "")
-			test_count += 1
-		for match in re.finditer(regex_detected, line, re.S):
-			success_count += 1
-	print success_count, "out of", test_count, "passed."
+    for line in f:
+        for match in re.finditer(regex_test, line, re.S):
+            # print line.replace(regex_test, "")
+            test_count += 1
+        for match in re.finditer(regex_detected, line, re.S):
+            success_count += 1
+    print(f"{success_count} out of {test_count} passed.")			
 
 # This is called automatically by running pytest in the same directory.
 def func_report():
-	return success_count
+    return success_count
 
 def test_success():
-	# define the pass threshold (60%)
-	assert func_report() >= test_count * .6
-	print success_count, " out of ", test_count, " passed."
+    # define the pass threshold (60%)
+    assert func_report() >= test_count * .6
+    print(f"{success_count} out of {test_count} passed.")			
