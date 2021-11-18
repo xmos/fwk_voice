b1408984997bbfc3cd00fe2959786d57e0a29901
johnshaferXMOS <johnshafer@xmos.com>
2021-11-18 16:03:11 -0500

    remove sudos

--- applications/avona/filesystem_support/create_fs.sh
+++ applications/avona/filesystem_support/create_fs.sh
@@ -7,14 +7,14 @@ else
         echo "fat_mnt directory already exists!"
     else
         # Create directory for intended files and Copy renamed files into directory
-        sudo mkdir -p fat_mnt/ww
-        sudo cp "$WW_PATH/models/common/WR_250k.en-US.alexa.bin" fat_mnt/ww/250kenUS.bin
-        sudo cp "$WW_PATH/models/common/WS_50k.en-US.alexa.bin" fat_mnt/ww/50kenUS.bin
+        mkdir -p fat_mnt/ww
+        cp "$WW_PATH/models/common/WR_250k.en-US.alexa.bin" fat_mnt/ww/250kenUS.bin
+        cp "$WW_PATH/models/common/WS_50k.en-US.alexa.bin" fat_mnt/ww/50kenUS.bin
 
         # Create env var for path to fatfs_mkimage?
         FATFS_MKIMAGE_PATH=../../host/fatfs
 
         # Run fatfs_mkimage.exe on the directory to create filesystem file
-        sudo $FATFS_MKIMAGE_PATH/fatfs_mkimage --input=fat_mnt/ww --output=fat.fs
+        $FATFS_MKIMAGE_PATH/fatfs_mkimage --input=fat_mnt/ww --output=fat.fs
     fi
 fi
