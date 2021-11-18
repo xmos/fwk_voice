0687f12c5998ddb131b6da2f74e4c4a7baf40988
johnshaferXMOS <johnshafer@xmos.com>
2021-11-18 16:11:03 -0500

    changed to nifty linux temp directory

--- applications/avona/filesystem_support/create_fs.sh
+++ applications/avona/filesystem_support/create_fs.sh
@@ -7,14 +7,14 @@ else
         echo "fat_mnt directory already exists!"
     else
         # Create directory for intended files and Copy renamed files into directory
-        mkdir -p fat_mnt/ww
-        cp "$WW_PATH/models/common/WR_250k.en-US.alexa.bin" fat_mnt/ww/250kenUS.bin
-        cp "$WW_PATH/models/common/WS_50k.en-US.alexa.bin" fat_mnt/ww/50kenUS.bin
+        tmp_dir=$(mktemp -d)
+        cp "$WW_PATH/models/common/WR_250k.en-US.alexa.bin" $tmp_dir/250kenUS.bin
+        cp "$WW_PATH/models/common/WS_50k.en-US.alexa.bin" $tmp_dir/50kenUS.bin
 
         # Create env var for path to fatfs_mkimage?
         FATFS_MKIMAGE_PATH=../../host/fatfs
 
         # Run fatfs_mkimage.exe on the directory to create filesystem file
-        $FATFS_MKIMAGE_PATH/fatfs_mkimage --input=fat_mnt/ww --output=fat.fs
+        $FATFS_MKIMAGE_PATH/fatfs_mkimage --input=$tmp_dir --output=fat.fs
     fi
 fi
