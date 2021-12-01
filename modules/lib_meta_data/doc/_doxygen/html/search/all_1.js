c159b699c523bc358ccca67d804e084b01a9c6a4
uvvpavel <paveluvarov@xmos.com>
2021-12-01 10:38:21 +0000

    removing _doxygen directory

--- modules/lib_meta_data/doc/_doxygen/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['meta_5fgen_2eh_2',['meta_gen.h',['../meta__gen_8h.html',1,'']]]
-];
--- modules/lib_meta_data/doc/_doxygen/html/search/functions_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['get_5faec_5fcorr_5ffactor_4',['get_aec_corr_factor',['../meta__gen_8h.html#a89e644dacda8eeabecf319874bc56e6d',1,'meta_gen.h']]],
-  ['get_5fmax_5fref_5fenergy_5',['get_max_ref_energy',['../meta__gen_8h.html#a558d921a28537b8c74ce478c96033eb1',1,'meta_gen.h']]]
-];
