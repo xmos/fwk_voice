533dda98273f02a7967305c1c92339756cac50f4
uvvpavel <paveluvarov@xmos.com>
2021-12-02 15:53:23 +0000

    adding noise suppression

---
+++ modules/lib_noise_suppression/api/suppression_conf.h
@@ -0,0 +1,16 @@
+// Copyright 2018-2021 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+#ifndef _suppression_conf_h
+#define _suppression_conf_h
+
+#define SUP_X_CHANNELS              (0)
+#define SUP_Y_CHANNELS              (2) // 1 means noise suppression applied only to ASR channel
+#define SUP_PROC_FRAME_LENGTH_LOG2  (9)
+#define SUP_FRAME_ADVANCE           (240)
+
+//#define SUP_DEBUG 1
+//#define SUP_DEBUG_PRINT 1
+//#define SUP_WARNING_PRINT 1
+
+
+#endif
