486ccf271edcfe9385ab0cf3de7022e97b190995
Daniel Pieczko <danielpieczko@xmos.com>
2021-12-16 17:08:18 +0000

    Implementation for lib_agc module

---
+++ modules/lib_agc/api/agc_api.h
@@ -0,0 +1,74 @@
+// Copyright 2021 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+#ifndef AGC_API_H
+#define AGC_API_H
+
+#include <xs3_math.h>
+#include <agc_init.h>
+
+// Length of the frame of data on which the AGC will operate
+#define AGC_FRAME_ADVANCE 240u
+
+typedef struct {
+    int adapt;
+    int adapt_on_vad;
+    int soft_clipping;
+    float_s32_t gain;
+    float_s32_t max_gain;
+    float_s32_t min_gain;
+    float_s32_t upper_threshold;
+    float_s32_t lower_threshold;
+    float_s32_t gain_inc;
+    float_s32_t gain_dec;
+    int lc_enabled;
+    int lc_n_frame_far;
+    int lc_n_frame_near;
+    float_s32_t lc_corr_threshold;
+    float_s32_t lc_bg_power_gamma;
+    float_s32_t lc_gamma_inc;
+    float_s32_t lc_gamma_dec;
+    float_s32_t lc_far_delta;
+    float_s32_t lc_near_delta;
+    float_s32_t lc_near_delta_far_act;
+    float_s32_t lc_gain_max;
+    float_s32_t lc_gain_dt;
+    float_s32_t lc_gain_silence;
+    float_s32_t lc_gain_min;
+} agc_config_t;
+
+typedef struct {
+    agc_config_t config;
+    float_s32_t x_slow;
+    float_s32_t x_fast;
+    float_s32_t x_peak;
+    int lc_t_far;
+    int lc_t_near;
+    float_s32_t lc_near_power_est;
+    float_s32_t lc_far_power_est;
+    float_s32_t lc_near_bg_power_est;
+    float_s32_t lc_gain;
+    float_s32_t lc_far_bg_power_est;
+    float_s32_t lc_corr_val;
+} agc_state_t;
+
+void agc_init(agc_state_t *agc, agc_config_t *config);
+
+typedef struct {
+    int vad_flag;
+    float_s32_t aec_ref_power;
+    float_s32_t aec_corr_factor;
+} agc_meta_data_t;
+
+// Definitions that can be used for assignments in agc_meta_data_t to make it clear to those
+// reading the code that the VAD or AEC are not present in the application.
+// If the VAD is not present, adapt_on_vad must be set to zero in agc_config_t
+#define AGC_META_DATA_NO_VAD 0u
+// If the AEC is not present, lc_enabled must be set to zero in agc_config_t
+#define AGC_META_DATA_NO_AEC (float_s32_t){0, 0}
+
+void agc_process_frame(agc_state_t *agc,
+                       int32_t output[AGC_FRAME_ADVANCE],
+                       const int32_t input[AGC_FRAME_ADVANCE],
+                       agc_meta_data_t *meta_data);
+
+#endif
---
+++ modules/lib_agc/api/agc_init.h
@@ -0,0 +1,89 @@
+// Copyright 2021 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+#ifndef AGC_INIT_H
+#define AGC_INIT_H
+
+#include <xs3_math.h>
+
+#define AGC_INITIALISER_ASR (agc_config_t){ \
+    .adapt = 1, \
+    .adapt_on_vad = 1, \
+    .soft_clipping = 1, \
+    .gain = float_to_float_s32(500), \
+    .max_gain = float_to_float_s32(1000), \
+    .min_gain = float_to_float_s32(0), \
+    .upper_threshold = float_to_float_s32(0.7079), \
+    .lower_threshold = float_to_float_s32(0.1905), \
+    .gain_inc = float_to_float_s32(1.197), \
+    .gain_dec = float_to_float_s32(0.87), \
+    .lc_enabled = 0, \
+    .lc_n_frame_far = 0, \
+    .lc_n_frame_near = 0, \
+    .lc_corr_threshold = float_to_float_s32(0), \
+    .lc_bg_power_gamma = float_to_float_s32(0), \
+    .lc_gamma_inc = float_to_float_s32(0), \
+    .lc_gamma_dec = float_to_float_s32(0), \
+    .lc_far_delta = float_to_float_s32(0), \
+    .lc_near_delta = float_to_float_s32(0), \
+    .lc_near_delta_far_act = float_to_float_s32(0), \
+    .lc_gain_max = float_to_float_s32(0), \
+    .lc_gain_dt = float_to_float_s32(0), \
+    .lc_gain_silence = float_to_float_s32(0), \
+    .lc_gain_min = float_to_float_s32(0), \
+    }
+
+#define AGC_INITIALISER_COMMS (agc_config_t){ \
+    .adapt = 1, \
+    .adapt_on_vad = 1, \
+    .soft_clipping = 1, \
+    .gain = float_to_float_s32(500), \
+    .max_gain = float_to_float_s32(1000), \
+    .min_gain = float_to_float_s32(0), \
+    .upper_threshold = float_to_float_s32(0.4), \
+    .lower_threshold = float_to_float_s32(0.4), \
+    .gain_inc = float_to_float_s32(1.0034), \
+    .gain_dec = float_to_float_s32(0.98804), \
+    .lc_enabled = 0, \
+    .lc_n_frame_far = 17, \
+    .lc_n_frame_near = 34, \
+    .lc_corr_threshold = float_to_float_s32(0.993), \
+    .lc_bg_power_gamma = float_to_float_s32(1.002), \
+    .lc_gamma_inc = float_to_float_s32(1.005), \
+    .lc_gamma_dec = float_to_float_s32(0.995), \
+    .lc_far_delta = float_to_float_s32(300), \
+    .lc_near_delta = float_to_float_s32(50), \
+    .lc_near_delta_far_act = float_to_float_s32(100), \
+    .lc_gain_max = float_to_float_s32(1), \
+    .lc_gain_dt = float_to_float_s32(0.9), \
+    .lc_gain_silence = float_to_float_s32(0.1), \
+    .lc_gain_min = float_to_float_s32(0.022387), \
+    }
+
+#define AGC_INITIALISER_FIXED_GAIN (agc_config_t){ \
+    .adapt = 0, \
+    .adapt_on_vad = 0, \
+    .soft_clipping = 0, \
+    .gain = float_to_float_s32(25), \
+    .max_gain = float_to_float_s32(0), \
+    .min_gain = float_to_float_s32(0), \
+    .upper_threshold = float_to_float_s32(0), \
+    .lower_threshold = float_to_float_s32(0), \
+    .gain_inc = float_to_float_s32(0), \
+    .gain_dec = float_to_float_s32(0), \
+    .lc_enabled = 0, \
+    .lc_n_frame_far = 0, \
+    .lc_n_frame_near = 0, \
+    .lc_corr_threshold = float_to_float_s32(0), \
+    .lc_bg_power_gamma = float_to_float_s32(0), \
+    .lc_gamma_inc = float_to_float_s32(0), \
+    .lc_gamma_dec = float_to_float_s32(0), \
+    .lc_far_delta = float_to_float_s32(0), \
+    .lc_near_delta = float_to_float_s32(0), \
+    .lc_near_delta_far_act = float_to_float_s32(0), \
+    .lc_gain_max = float_to_float_s32(0), \
+    .lc_gain_dt = float_to_float_s32(0), \
+    .lc_gain_silence = float_to_float_s32(0), \
+    .lc_gain_min = float_to_float_s32(0), \
+    }
+
+#endif
