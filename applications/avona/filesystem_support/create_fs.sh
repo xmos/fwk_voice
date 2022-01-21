1f0209d1cd28623def29ebd47959ecb90c2f084d
Keith Moulton <keithm@xmos.com>
2022-01-21 16:36:40 -0500

    commented out develop branch trigger for now

--- applications/avona/filesystem_support/create_fs.sh
+++ applications/avona/filesystem_support/create_fs.sh
@@ -8,7 +8,7 @@ cp "$WW_PATH/models/common/WR_250k.en-US.alexa.bin" $ww_dir/250kenUS.bin
 cp "$WW_PATH/models/common/WS_50k.en-US.alexa.bin" $ww_dir/50kenUS.bin
 
 # Create env var for path to fatfs_mkimage?
-FATFS_MKIMAGE_PATH=../../host/fatfs
+FATFS_MKIMAGE_PATH=/opt/xmos/SDK/0.11.1/bin/
 
 # Run fatfs_mkimage.exe on the directory to create filesystem file
 $FATFS_MKIMAGE_PATH/fatfs_mkimage --input=$tmp_dir --output=fat.fs
