be3a4da0bbba576391868f5c656fc43fba70a086
Ed <ed@xmos.com>
2022-01-24 12:03:50 +0000

    Fix unit test speedup

--- test/lib_ic/ic_unit_tests/src/ic_unit_tests.h
+++ test/lib_ic/ic_unit_tests/src/ic_unit_tests.h
@@ -36,9 +36,7 @@ extern "C" {
 // Set F to a power of 2 greater than 1 to speedup testing by a Fx
 #undef F
 #ifdef SPEEDUP_FACTOR
-    //TODO Fix
-    // #define F (SPEEDUP_FACTOR)
-    #define F 1
+    #define F (SPEEDUP_FACTOR)
 #else
     #define F 1
 #endif
