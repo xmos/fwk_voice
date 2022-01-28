970e2635770172fb83461568ff37b4ea07086bc9
uvvpavel <paveluvarov@xmos.com>
2022-01-28 16:56:06 +0000

    bug fixes

--- test/lib_noise_suppression/compare_c_py/test_compare_c_py.py
+++ test/lib_noise_suppression/compare_c_py/test_compare_c_py.py
@@ -6,8 +6,9 @@ from __future__ import print_function
 #standard_library.install_aliases()
 from builtins import zip
 import pytest
-from compare_c_py import get_attenuation_c_py
+from compare_c_py import get_attenuation_c_xc
 import configparser
+import numpy as np
 
 
 def read_config(testname, filename):
@@ -16,28 +17,20 @@ def read_config(testname, filename):
     cfg = {}
     cfg['noise_db'] = [int(x) for x in parser.get(testname, "noise_db").split(',')]
     cfg['noise_band'] = [x.strip() for x in parser.get(testname, "noise_band").split(',')]
-    cfg['py_atten'] = [x.strip() for x in parser.get(testname, "py_atten").split(',')]
     return cfg
 
 
 def get_test_instances(testname='gen_audio', filename='config.cfg'):
     config = read_config(testname, filename)
     tests = []
-    i = 0
     for db in config['noise_db']:
         for noise in config['noise_band']:
             test_id = "{}_{}Hz_{}dB".format(testname, noise, db)
 
-            arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-            for t in range (i, i + 10):
-                arr[t - i] = config['py_atten'][t]
-
             test_dict = {'id' : test_id,
                              'noise_band' : noise,
-                             'noise_db' : db,
-                             'attenuation_py' : arr}
+                             'noise_db' : db}
             tests.append(test_dict)
-            i = i + 10
     return tests
 
 
@@ -47,9 +40,10 @@ TESTS = get_test_instances('gen_audio', 'config.cfg')
 def test_sup_attenuation(test):
     print("--- {} ---" .format(test['id']))
 
-    attenuation_c = get_attenuation_c_py(test['id'], int(test['noise_band']), int(test['noise_db']))
+    attenuation_c, attenuation_xc = get_attenuation_c_xc(test['id'], int(test['noise_band']), int(test['noise_db']))
 
-    print("PYTHON SUP: {}".format(["%.2f"%item for item in test['attenuation_py']]))
-    for (atten_bin_xc, atten_bin_py) in zip(attenuation_c, test['attenuation_py']):
-        assert abs(atten_bin_xc - atten_bin_py) < 1
-        assert atten_bin_py > 0
+    c_res = np.array(attenuation_c)
+    xc_res = np.array(attenuation_xc)
+    for (atten_bin_xc, atten_bin_c) in zip(xc_res, c_res):
+        assert atten_bin_xc - 1 < atten_bin_c
+        assert atten_bin_c > 0
