76999ecb27885383d5c493845ed01b8293e6d8a7
Shuchita Khare <shuchitak@Shuchitas-MacBook-Pro.local>
2022-02-01 12:16:18 +0000

    more plotting changes

--- test/lib_adec/test_wav_adec/plot.py
+++ test/lib_adec/test_wav_adec/plot.py
@@ -49,12 +49,15 @@ def test():
     c_logs.erle_ratio = get_erle_ratio(c_logs.ema_y, c_logs.ema_error)
 
     #Read xc output
-    num_fields_xc = 3
+    num_fields_xc = 6
     l = np.array(np.loadtxt("xc_log.txt"), dtype=np.float64)
     deinterleaved = [l[i::num_fields_xc] for i in range(num_fields_xc)]
     xc_logs.ema_error = np.array(deinterleaved[0], dtype=np.float64)
     xc_logs.ema_y = np.array(deinterleaved[1], dtype=np.float64)
     xc_logs.shadow_flag = np.array(deinterleaved[2], dtype=np.int32)
+    xc_logs.ov_Error = np.array(deinterleaved[3], dtype=np.float64)
+    xc_logs.ov_Error_shad = np.array(deinterleaved[4], dtype=np.float64)
+    xc_logs.ov_Y = np.array(deinterleaved[5], dtype=np.float64)
     xc_logs.erle_ratio = get_erle_ratio(xc_logs.ema_y, xc_logs.ema_error)
 
     #Read python output
@@ -74,19 +77,22 @@ def test():
     axes[0,0].set_ylim(0,7)
     axes[0,0].plot([i for i in range(len(c_logs.erle_ratio))], c_logs.erle_ratio[:], color='b')
     axes[0,0].plot([i for i in range(len(py_logs.erle_ratio))], py_logs.erle_ratio[:], color='g')
-    axes[0,0].plot([i for i in range(len(xc_logs.erle_ratio))], xc_logs.erle_ratio[:], color='m')
+    axes[0,0].plot([i for i in range(len(xc_logs.erle_ratio))], xc_logs.erle_ratio[:], 'm--')
 
     axes[0,1].set_title("ov_Error_shad")
     axes[0,1].plot([i for i in range(len(c_logs.ov_Error_shad))], c_logs.ov_Error_shad, color='b')
     axes[0,1].plot([i for i in range(len(py_logs.ov_Error_shad))], py_logs.ov_Error_shad, color='g')
+    axes[0,1].plot([i for i in range(len(xc_logs.ov_Error_shad))], xc_logs.ov_Error_shad, 'm--')
 
     axes[0,2].set_title("ov_Error")
     axes[0,2].plot([i for i in range(len(c_logs.ov_Error))], c_logs.ov_Error, color='b')
     axes[0,2].plot([i for i in range(len(py_logs.ov_Error))], py_logs.ov_Error, color='g')
+    axes[0,2].plot([i for i in range(len(xc_logs.ov_Error))], xc_logs.ov_Error, 'm--')
 
     axes[0,3].set_title("ov_Y")
     axes[0,3].plot([i for i in range(len(c_logs.ov_Y))], c_logs.ov_Y, color='b')
     axes[0,3].plot([i for i in range(len(py_logs.ov_Y))], py_logs.ov_Y, color='g')
+    axes[0,3].plot([i for i in range(len(xc_logs.ov_Y))], xc_logs.ov_Y, 'm--')
 
     axes[1,0].set_title("shadow_better_count")
     axes[1,0].plot([i for i in range(len(c_logs.shadow_better_count))], c_logs.shadow_better_count, color='b')
@@ -99,7 +105,7 @@ def test():
     axes[1,2].set_title("shadow_flag")
     axes[1,2].plot([i for i in range(len(c_logs.shadow_flag))], c_logs.shadow_flag, 'b')
     axes[1,2].plot([i for i in range(len(py_logs.shadow_flag))], py_logs.shadow_flag, 'g')
-    axes[1,2].plot([i for i in range(len(xc_logs.shadow_flag))], xc_logs.shadow_flag, 'm')
+    axes[1,2].plot([i for i in range(len(xc_logs.shadow_flag))], xc_logs.shadow_flag, 'm--')
 
     axes[1,3].set_title("peak_to_avg_ratio")
     axes[1,3].plot([i for i in range(len(c_logs.peak_to_avg_ratio))], c_logs.peak_to_avg_ratio, color='b')
