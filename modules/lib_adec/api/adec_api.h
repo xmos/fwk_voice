141e6c573e521e3d174c62a73cb1e7d59ca406a3
Shuchita Khare <shuchitak@Shuchitas-MacBook-Pro.local>
2022-02-09 12:52:16 +0000

    Added docmentation for adec_init()

--- modules/lib_adec/api/adec_api.h
+++ modules/lib_adec/api/adec_api.h
@@ -2,7 +2,42 @@
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 #include "adec_state.h"
 
-/** Function that initialises ADEC state
+/**
+ * @page page_adec_api_h adec_api.h
+ * 
+ * lib_adec public functions API.
+ *
+ * @ingroup adec_header_file
+ */
+
+/**
+ * @defgroup adec_func     ADEC API Functions
+ */
+
+/** @brief Initialise ADEC data structures
+ * 
+ * This function initialises ADEC state for a given configuration. It must be called at startup to initialise the ADEC
+ * data structures before processing any frames, and can be called at any time after that to reset the ADEC instance, returning
+ * the internal ADEC state to its defaults.
+ * 
+ * @param[out] state Pointer to ADEC state structure
+ * @param[in] config Pointer to ADEC configuration structure.
+ * 
+ * @par Example with ADEC configured for delay estimation only at startup
+ * @code{.c}
+ *      adec_state_t adec_state;
+        adec_config_t adec_conf = ADEC_CONFIG_DE_ONLY_AT_STARTUP;
+        adec_init(&adec_state, &adec_conf);
+ * @endcode
+ *
+ * @par Example with ADEC configured for automatic delay estimation and control
+ * @code{.c}
+ *      adec_state_t adec_state;
+        adec_config_t adec_conf = ADEC_CONFIG_AUTOMATIC_DE_CONTROL;
+        adec_init(&adec_state, &adec_conf);
+ * @endcode
+ *
+ * @ingroup adec_func
  */
 void adec_init(adec_state_t *state, adec_config_t *config);
 
