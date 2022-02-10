daedfa206fe9b5947f9c8c1db79efe6819a74687
Ed <ed@xmos.com>
2022-02-10 11:25:38 +0000

    WIP add frame compare test

---
+++ test/lib_vad/compare_xc_c/src/main.c
@@ -0,0 +1,10 @@
+// Copyright 2022 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
+#include <stdio.h>
+#include <assert.h>
+
+
+int main(void){
+    printf("compare XC C\n");
+}
