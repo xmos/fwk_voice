074477d0e8d2878768823b9fdb92efae7acf6807
Ed <ed@xmos.com>
2022-02-18 11:42:42 +0000

    test mel now correctly fails

--- test/lib_vad/vad_unit_tests/src/test_compare_mel_compute.c
+++ test/lib_vad/vad_unit_tests/src/test_compare_mel_compute.c
@@ -6,6 +6,7 @@
 #include "vad_parameters.h"
 #include "bfp_math.h"
 #include "dsp.h"
+#include <string.h>
 
 //Note this test assumes that test_compare_fft has worked as we use vad_xs3_math_fft to generate vectors
 
@@ -28,36 +29,56 @@ int iabs(int a, int b){
 void test_compare_mel(){
     unsigned seed = 6031759;
 
-    dsp_complex_t DWORD_ALIGNED in_vector[VAD_PROC_FRAME_LENGTH + 2];
+    complex_s32_t DWORD_ALIGNED in_vector[VAD_PROC_FRAME_LENGTH + 2];
+    complex_s32_t DWORD_ALIGNED in_vector_copy[VAD_PROC_FRAME_LENGTH + 2];
     int32_t old_mel[VAD_N_MEL_SCALE + 1] = {0};
     int32_t new_mel[VAD_N_MEL_SCALE + 1] = {0};
     int32_t max = 0x0000000f;
 
+    int test_passed = 1;
+
     for(int i = 0; i < 100; i++){ //100 gets us to very near full scale
+        for(int t = 0; t < 2; t++){ //Gen FFTd data then chuck in raw data to get better coverage
 
-        if((i % 12 ) == 0) max = max<<3;
+            if((i % 7 ) == 0) max = max<<1;
 
-        for(int v = 0; v < VAD_PROC_FRAME_LENGTH; v++){
-            in_vector[v].re = pseudo_rand_int(&seed, -max, max);
-            in_vector[v].im = 0;
-        }
+            headroom_t headroom = 0;
 
-        // printf("val 0 - %d\n", in_vector[0].re);
+            if(t == 0){
+                for(int v = 0; v < VAD_PROC_FRAME_LENGTH; v++){
+                    in_vector[v].re = pseudo_rand_int(&seed, -max, max);
+                    in_vector[v].im = 0;
+                }
 
-        headroom_t headroom = vad_xs3_math_fft(in_vector, 1);
+                headroom = vad_xs3_math_fft((int32_t *)in_vector, 1);
+                printf("headroom fftdata: %u\n", headroom);
+            }else{
+                for(int v = 0; v < VAD_PROC_FRAME_LENGTH; v++){
+                    in_vector[v].re = pseudo_rand_int(&seed, -max, max);
+                    in_vector[v].im = pseudo_rand_int(&seed, -max, max);;
+                }
 
-        // printf("HERE1 %d %d\n", in_vector[0].re, in_vector[0].im);
-        vad_mel_compute_new(new_mel, VAD_N_MEL_SCALE + 1, in_vector, VAD_PROC_FRAME_BINS + 1, vad_mel_table24_512, 2*VAD_LOG_WINDOW_LENGTH-2*headroom);
-        // printf("HERE2 %d %d\n", in_vector[0].re, in_vector[0].im);
-        vad_mel_compute(old_mel, VAD_N_MEL_SCALE + 1, in_vector, VAD_PROC_FRAME_BINS + 1, vad_mel_table24_512, 2*VAD_LOG_WINDOW_LENGTH-2*headroom);
-        // printf("HERE3 %d %d\n", in_vector[0].re, in_vector[0].im);
+                headroom = xs3_vect_s32_headroom((int32_t*)in_vector, VAD_PROC_FRAME_LENGTH * 2);
+                printf("headroom rawdata: %u\n", headroom);
+            }
 
-        int max_diff = 100;
-        for(int v = 0; v < VAD_N_MEL_SCALE + 1; v++){
-            if(iabs(old_mel[v], new_mel[v]) > max_diff){
-                printf("mel: %d: old: %ld new: %ld\n", v, old_mel[v], new_mel[v]);
-                TEST_ASSERT(0);
+            memcpy(in_vector_copy, in_vector, sizeof(in_vector));
+
+            // printf("HERE1 %d %d\n", in_vector[0].re, in_vector[0].im);
+            // vad_mel_compute(new_mel, VAD_N_MEL_SCALE + 1, in_vector, VAD_PROC_FRAME_BINS + 1, vad_mel_table24_512, 2*VAD_LOG_WINDOW_LENGTH-2*headroom);
+            vad_mel_compute_new(new_mel, VAD_N_MEL_SCALE + 1, in_vector, VAD_PROC_FRAME_BINS + 1, vad_mel_table24_512, 2*VAD_LOG_WINDOW_LENGTH-2*headroom);
+            // printf("HERE2 %d %d\n", in_vector[0].re, in_vector[0].im);
+            vad_mel_compute(old_mel, VAD_N_MEL_SCALE + 1, in_vector_copy, VAD_PROC_FRAME_BINS + 1, vad_mel_table24_512, 2*VAD_LOG_WINDOW_LENGTH-2*headroom);
+            // printf("HERE3 %d %d\n", in_vector[0].re, in_vector[0].im);
+
+            int max_diff = 100;
+            for(int v = 0; v < VAD_N_MEL_SCALE + 1; v++){
+                if(iabs(old_mel[v], new_mel[v]) > max_diff){
+                    printf("mel: %d: old: %ld new: %ld\n", v, old_mel[v], new_mel[v]);
+                    test_passed = 0;
+                }
             }
         }
     }
+    TEST_ASSERT(test_passed);
 }
