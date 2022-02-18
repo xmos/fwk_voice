656e2ce3a57fa3632a71190abacc4e25f4fb6307
Ed <ed@xmos.com>
2022-02-18 10:43:01 +0000

    Extend test utilities in compare xc c

--- test/lib_vad/compare_xc_c/test_compare_xc_c.py
+++ test/lib_vad/compare_xc_c/test_compare_xc_c.py
@@ -55,6 +55,50 @@ def dump_both(old, new):
     for old_line, new_line in zip(old, new):
         print(old_line.strip(), "\n", new_line.strip())
 
+
+def how_close(a, b):
+    abs_diff = abs(a-b)
+    if b == 0 or a == 0:
+        rel = 9999999999
+    else:
+        if b > a:
+            rel = b/a - 1.0
+        else:
+            rel = a/b - 1.0
+    return rel, abs_diff
+
+
+def is_close(a, b):
+    rel_diff, abs_diff = how_close(a, b)
+
+    if rel_diff < 0.00001 or abs_diff <= 2:
+        return True 
+    else:
+        return False 
+
+
+def compare_lines(old, new):
+    frame = 0
+    for old_line, new_line in zip(old, new):
+        if not "MEL" in old_line:
+            continue
+        old_line = old_line.replace("[DEVICE] MEL ", "")
+        new_line = new_line.replace("[DEVICE] MEL ", "")
+        # print("**", old_line, new_line)
+        old_vals = [s for s in old_line.split(' ') if s] #collapse conseqcuitive delimiters
+        new_vals = [s for s in new_line.split(' ') if s]
+
+        binc = 0
+        for old_val, new_val in zip(old_vals, new_vals):
+            if old_val == "\n":
+                continue
+            old = float(old_val)
+            new = float(new_val)
+            if not is_close(old, new):
+                print(f"Disparity at frame {frame} bin {binc} old {old} new {new} :", how_close(old, new))
+            binc += 1
+        frame += 1
+
 def test_xc_c_comparison():
     stdo_old_vad = process_xe(old_vad_xe, input_wav)
     stdo_new_vad = process_xe(new_vad_xe, input_wav)
