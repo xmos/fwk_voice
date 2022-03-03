66157fc078a554ca2affdce3a595efcde8a8c8a4
Ed <ed@xmos.com>
2022-03-03 15:00:15 +0000

    Produce two separate, sorted results files

--- test/pipeline/conftest.py
+++ test/pipeline/conftest.py
@@ -10,7 +10,7 @@ pipeline_output_base_dir = os.path.abspath("./pipeline_output/")
 keyword_input_base_dir = os.path.abspath("./keyword_input/")
 pipeline_x86_bin = os.path.abspath("../../build/examples/bare-metal/pipeline_single_threaded/bin/avona_example_bare_metal_pipeline_single_thread")
 pipeline_xe_bin = os.path.abspath("../../build/examples/bare-metal/pipeline_single_threaded/bin/avona_example_bare_metal_pipeline_single_thread.xe")
-results_log_file = os.path.abspath("pipeline_results.txt")
+results_log_file = os.path.abspath("results.txt")
 
 all_tests_list = []
 targets = ("xcore", "x86")
@@ -53,15 +53,26 @@ def pytest_sessionstart(session):
     open(results_log_file, "w").close()
 
 def pytest_sessionfinish(session):
-    with open(results_log_file, "r+") as lf:
+    #read log file
+    with open(results_log_file, "r") as lf:
         log = lf.readlines()
-        log = sorted(log)
-        lf.seek(0)
-        lf.writelines(log)
+        #split into two, target specific, sorted files
+        for target in targets:
+            target_log = []
+            for line in log:
+                if target in line:
+                    target_stripped_line = line.replace(target+",", "")
+                    target_log.append(target_stripped_line) 
+            target_log = sorted(target_log)
+            target_specific_log_file = results_log_file.replace(".txt", "_"+target+".txt")
+            with open(target_specific_log_file, "w") as tlf:
+                tlf.writelines(target_log)
+
+
 
 
 def pytest_generate_tests(metafunc):
     ids = [os.path.basename(item[0]) + ", " + item[1] for item in all_tests_list]
-    metafunc.parametrize("test", all_tests_list, ids=ids)
-    # n_procs = 8
-    # metafunc.parametrize("test_audio", all_files_list[0:n_procs], ids=all_files_list[0:n_procs]) #just a few
+    # metafunc.parametrize("test", all_tests_list, ids=ids)
+    n_procs = 8
+    metafunc.parametrize("test", all_tests_list[0:n_procs], ids=ids[0:n_procs]) #just a few
