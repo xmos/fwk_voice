4dc3c77cad507afa69225458d2787f97d7000050
Ed <ed@xmos.com>
2022-03-11 18:15:30 +0000

    Add test_wav_ic style loop

--- test/lib_ic/py_c_frame_compare/test_ic_frame.py
+++ test/lib_ic/py_c_frame_compare/test_ic_frame.py
@@ -2,6 +2,8 @@
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
 import numpy as np
+import scipy.io.wavfile
+import audio_wav_utils as awu
 import ctypes
 import pytest
 import sys, os
@@ -18,17 +20,24 @@ sys.path.append(att_path)
 sys.path.append(py_ic_path)
 import IC
 
+
+proc_frame_length = 512
+frame_advance = 240
+num_phases = 10
+input_file = "../../../examples/bare-metal/ic/input.wav"
+output_file = "output.wav"
+
 @pytest.fixture(params=[33,34])
 def pre_test_stuff(request):
-   
     return request.param
 
 
 def float_s32_to_float(float_s32):
     return np.ldexp(float_s32.mant, float_s32.exp)
 
-def a_func(inval):
-    ic = IC.adaptive_interference_canceller(240, 512, 10, 0, 
+class ic_comparison:
+    def __init__(self):
+        self.ic = IC.adaptive_interference_canceller(frame_advance, proc_frame_length, num_phases, 0, 
         mu = 0.36956599983386695,
         delta = 6.999999999379725e-05,
         delay = 0,
@@ -41,36 +50,55 @@ def a_func(inval):
         use_noise_minimisation = False,
         output_beamform_on_ch1 = True)
 
-    frame = np.ones((2, 240), dtype=np.int32) * 10000000
-    frame_fp = frame.astype(np.float64) / (-np.iinfo(np.int32).min)
-    all_channels_output, Error_ap = ic.process_frame(frame_fp)
-    ic.adapt(Error_ap)
-    output_py = all_channels_output[0]
+        ic_test_lib.test_init()
 
-    ic_test_lib.test_init()
+    def process_frame(self, frame):
+        frame_fp = frame.astype(np.float64) / (-np.iinfo(np.int32).min)
+        all_channels_output, Error_ap = self.ic.process_frame(frame_fp)
+        self.ic.adapt(Error_ap)
+        output_py = all_channels_output[0]
 
-    state = ic_test_lib.test_get_state()
-    print(float_s32_to_float(state.mu[0][0]))
-    print(float_s32_to_float(state.config_params.delta))
+        output_c = np.zeros((240), dtype=np.int32)
+        y_data = ffi.from_buffer(frame[0].data)
+        x_data = ffi.from_buffer(frame[1].data)
+        ic_test_lib.test_filter(y_data, x_data, ffi.from_buffer(output_c.data))
+        vad = int(0)
+        ic_test_lib.test_adapt(0, ffi.from_buffer(output_c.data))
+        output_c = output_c.astype(np.float64) / (-np.iinfo(np.int32).min)
 
-    c_output = np.zeros((240))
-    print("&***",frame[0].ctypes.data_as(ctypes.POINTER(ctypes.c_int32)))
+        return output_py, output_c
 
-    y_data = ffi.from_buffer(frame[0].data)
-    x_data = ffi.from_buffer(frame[1].data)
-    output = np.zeros(240, dtype=np.int32);
-    ic_test_lib.test_filter(y_data, x_data, ffi.from_buffer(output.data))
-    
-    vad = int(0)
-    ic_test_lib.test_adapt(0, ffi.from_buffer(output.data))
-    output_c = output.astype(np.float64) / (-np.iinfo(np.int32).min)
-
-    print(output_py) 
-    print(output_c) 
+    # state = ic_test_lib.test_get_state()
+    # print(float_s32_to_float(state.mu[0][0]))
+    # print(float_s32_to_float(state.config_params.delta))
 
 
-def test_frame(pre_test_stuff):
+def test_frame_compare(pre_test_stuff):
+    icc = ic_comparison()
 
-    a_func(pre_test_stuff)
+
+    input_rate, input_wav_file = scipy.io.wavfile.read(input_file, 'r')
+    input_wav_data, input_channel_count, file_length = awu.parse_audio(input_wav_file)
+
+    for frame_start in range(0, file_length-proc_frame_length*2, frame_advance):
+        input_frame = awu.get_frame(input_wav_data, frame_start, frame_advance, np.zeros(2))
+
+        if False:
+            print ('# ' + str(frame_start // frame_advance))
+
+        output_py, output_c = icc.process_frame(input_frame)
+        current_output = output_py
+
+        # output[:, frame_start: frame_start + frame_advance] = current_output
+        print(frame_start)   
+        print(output_py.shape, output_c.shape)
+
+
+
+
+    # output_32bit = np.asarray(output*np.iinfo(np.int32).max, dtype=np.int32)
+    # scipy.io.wavfile.write(output_file, input_rate, output_32bit.T)
+
+
 
 
