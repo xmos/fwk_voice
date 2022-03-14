0a2aaeb08dd6d4d5c6c99ba0963cb53b181cab17
Ed <ed@xmos.com>
2022-03-14 10:01:20 +0000

    Fix types in ic process frame

--- test/lib_ic/py_c_frame_compare/test_ic_frame.py
+++ test/lib_ic/py_c_frame_compare/test_ic_frame.py
@@ -27,13 +27,21 @@ num_phases = 10
 input_file = "../../../examples/bare-metal/ic/input.wav"
 output_file = "output.wav"
 
-@pytest.fixture(params=[33,34])
+@pytest.fixture(params=[34])
 def pre_test_stuff(request):
     return request.param
 
 
 def float_s32_to_float(float_s32):
     return np.ldexp(float_s32.mant, float_s32.exp)
+
+def int32_to_float(array_int32):
+    array_float = array_int32.astype(np.float64) / (-np.iinfo(np.int32).min)
+    return array_float
+
+def float_to_int32(array_float):
+    array_int32 = (array_float * np.iinfo(np.int32).max).astype(np.int32)
+    return array_int32
 
 class ic_comparison:
     def __init__(self):
@@ -53,20 +61,20 @@ class ic_comparison:
         ic_test_lib.test_init()
 
     def process_frame(self, frame):
-        frame_fp = frame.astype(np.float64) / (-np.iinfo(np.int32).min)
-        all_channels_output, Error_ap = self.ic.process_frame(frame_fp)
+        all_channels_output, Error_ap = self.ic.process_frame(frame)
         self.ic.adapt(Error_ap)
-        output_py = all_channels_output[0]
+        output_py = all_channels_output[0,:]
 
+        frame_int = float_to_int32(frame)
+        y_data = ffi.from_buffer(frame_int[0].data)
+        x_data = ffi.from_buffer(frame_int[1].data)
         output_c = np.zeros((240), dtype=np.int32)
-        y_data = ffi.from_buffer(frame[0].data)
-        x_data = ffi.from_buffer(frame[1].data)
-        ic_test_lib.test_filter(y_data, x_data, ffi.from_buffer(output_c.data))
+        output_c_ptr = ffi.cast("int32_t *", ffi.from_buffer(output_c.data))
+        ic_test_lib.test_filter(y_data, x_data, output_c_ptr)
         vad = int(0)
-        ic_test_lib.test_adapt(0, ffi.from_buffer(output_c.data))
-        output_c = output_c.astype(np.float64) / (-np.iinfo(np.int32).min)
+        ic_test_lib.test_adapt(0, output_c_ptr)
 
-        return output_py, output_c
+        return output_py, int32_to_float(output_c)
 
     # state = ic_test_lib.test_get_state()
     # print(float_s32_to_float(state.mu[0][0]))
@@ -80,6 +88,8 @@ def test_frame_compare(pre_test_stuff):
     input_rate, input_wav_file = scipy.io.wavfile.read(input_file, 'r')
     input_wav_data, input_channel_count, file_length = awu.parse_audio(input_wav_file)
 
+    output_wav_data = np.zeros((2, file_length))
+
     for frame_start in range(0, file_length-proc_frame_length*2, frame_advance):
         input_frame = awu.get_frame(input_wav_data, frame_start, frame_advance, np.zeros(2))
 
@@ -87,17 +97,11 @@ def test_frame_compare(pre_test_stuff):
             print ('# ' + str(frame_start // frame_advance))
 
         output_py, output_c = icc.process_frame(input_frame)
-        current_output = output_py
 
-        # output[:, frame_start: frame_start + frame_advance] = current_output
-        print(frame_start)   
-        print(output_py.shape, output_c.shape)
-
-
-
-
-    # output_32bit = np.asarray(output*np.iinfo(np.int32).max, dtype=np.int32)
-    # scipy.io.wavfile.write(output_file, input_rate, output_32bit.T)
+        output_wav_data[0, frame_start: frame_start + frame_advance] = output_py
+        output_wav_data[1, frame_start: frame_start + frame_advance] = output_c
+        
+    scipy.io.wavfile.write(output_file, input_rate, output_wav_data.T)
 
 
 
