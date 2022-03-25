82c423e24e302971b1462d5a9089135de479d789
Ed <ed@xmos.com>
2022-03-25 10:22:56 +0000

    Make new AGC settings standard

--- modules/lib_agc/api/agc_profiles.h
+++ modules/lib_agc/api/agc_profiles.h
@@ -31,35 +31,8 @@
     .gain = float_to_float_s32(500), \
     .max_gain = float_to_float_s32(1000), \
     .min_gain = float_to_float_s32(0), \
-    .upper_threshold = float_to_float_s32(0.7079), \
-    .lower_threshold = float_to_float_s32(0.1905), \
-    .gain_inc = float_to_float_s32(1.197), \
-    .gain_dec = float_to_float_s32(0.87), \
-    .lc_enabled = 0, \
-    .lc_n_frame_far = 0, \
-    .lc_n_frame_near = 0, \
-    .lc_corr_threshold = float_to_float_s32(0), \
-    .lc_bg_power_gamma = float_to_float_s32(0), \
-    .lc_gamma_inc = float_to_float_s32(0), \
-    .lc_gamma_dec = float_to_float_s32(0), \
-    .lc_far_delta = float_to_float_s32(0), \
-    .lc_near_delta = float_to_float_s32(0), \
-    .lc_near_delta_far_active = float_to_float_s32(0), \
-    .lc_gain_max = float_to_float_s32(0), \
-    .lc_gain_double_talk = float_to_float_s32(0), \
-    .lc_gain_silence = float_to_float_s32(0), \
-    .lc_gain_min = float_to_float_s32(0), \
-    }
-
-#define AGC_PROFILE_ASR_HIGH (agc_config_t){ \
-    .adapt = 1, \
-    .adapt_on_vad = 1, \
-    .soft_clipping = 1, \
-    .gain = float_to_float_s32(500), \
-    .max_gain = float_to_float_s32(1000), \
-    .min_gain = float_to_float_s32(0), \
-    .upper_threshold = float_to_float_s32(0.99999), \
-    .lower_threshold = float_to_float_s32(0.70000), \
+    .upper_threshold = float_to_float_s32(0.9999), \
+    .lower_threshold = float_to_float_s32(0.7000), \
     .gain_inc = float_to_float_s32(1.197), \
     .gain_dec = float_to_float_s32(0.87), \
     .lc_enabled = 0, \
@@ -88,33 +61,6 @@
     .adapt_on_vad = 0, \
     .soft_clipping = 0, \
     .gain = float_to_float_s32(25), \
-    .max_gain = float_to_float_s32(0), \
-    .min_gain = float_to_float_s32(0), \
-    .upper_threshold = float_to_float_s32(0), \
-    .lower_threshold = float_to_float_s32(0), \
-    .gain_inc = float_to_float_s32(0), \
-    .gain_dec = float_to_float_s32(0), \
-    .lc_enabled = 0, \
-    .lc_n_frame_far = 0, \
-    .lc_n_frame_near = 0, \
-    .lc_corr_threshold = float_to_float_s32(0), \
-    .lc_bg_power_gamma = float_to_float_s32(0), \
-    .lc_gamma_inc = float_to_float_s32(0), \
-    .lc_gamma_dec = float_to_float_s32(0), \
-    .lc_far_delta = float_to_float_s32(0), \
-    .lc_near_delta = float_to_float_s32(0), \
-    .lc_near_delta_far_active = float_to_float_s32(0), \
-    .lc_gain_max = float_to_float_s32(0), \
-    .lc_gain_double_talk = float_to_float_s32(0), \
-    .lc_gain_silence = float_to_float_s32(0), \
-    .lc_gain_min = float_to_float_s32(0), \
-    }
-
-#define AGC_PROFILE_FIXED_GAIN_HIGH (agc_config_t){ \
-    .adapt = 0, \
-    .adapt_on_vad = 0, \
-    .soft_clipping = 0, \
-    .gain = float_to_float_s32(1000), \
     .max_gain = float_to_float_s32(0), \
     .min_gain = float_to_float_s32(0), \
     .upper_threshold = float_to_float_s32(0), \
