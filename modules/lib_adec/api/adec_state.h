2ff8bece0d21aa89e55cb62141eb5d2442a69ad1
Shuchita Khare <shuchitak@xmos.com>
2022-03-31 16:28:45 +0100

    Updated Copyright date

--- modules/lib_adec/api/adec_state.h
+++ modules/lib_adec/api/adec_state.h
@@ -1,4 +1,4 @@
-// Copyright 2020-2021 XMOS LIMITED.
+// Copyright 2022 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 #ifndef __ADEC_TYPES_H__
 #define __ADEC_TYPES_H__
--- modules/lib_agc/api/agc_api.h
+++ modules/lib_agc/api/agc_api.h
@@ -1,4 +1,4 @@
-// Copyright 2021-2022 XMOS LIMITED.
+// Copyright 2022 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 #ifndef AGC_API_H
 #define AGC_API_H
