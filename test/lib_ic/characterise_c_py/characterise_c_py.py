aae050a5f5689ebfe59ec37343c5821c54139415
uvvpavel <paveluvarov@xmos.com>
2022-04-27 15:25:19 +0100

    testing other test

--- test/lib_ic/characterise_c_py/characterise_c_py.py
+++ test/lib_ic/characterise_c_py/characterise_c_py.py
@@ -11,7 +11,7 @@ import tempfile
 
 package_dir = os.path.dirname(os.path.abspath(__file__))
 att_path = os.path.join(package_dir,'../../../audio_test_tools/python/')
-py_ic_path = os.path.join(package_dir,'../../../../lib_interference_canceller/python')
+py_ic_path = os.path.join(package_dir,'../../../../py_ic/py_ic')
 
 sys.path.append(att_path)
 sys.path.append(py_ic_path)
