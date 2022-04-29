d69a4590e285b7350b5bd2ccb91a4f8c258b7389
Shuchita Khare <shuchitak@xmos.com>
2022-04-29 12:45:23 +0100

    Added host file

--- examples/bare-metal/vnr/src/main.c
+++ examples/bare-metal/vnr/src/main.c
@@ -18,8 +18,10 @@ void get_frame(chanend_t xscope_chan, chanend_t c_frame_out){
   int32_t data[240];
   int bytes_read = 0;
   int input_bytes = 0;
+  printf("in get frame 1\n");
   xscope_mode_lossless();
   xscope_connect_data_from_host(xscope_chan);
+  printf("in get frame 2\n");
   while(1){
     SELECT_RES(CASE_THEN(xscope_chan, read_host_data)){
       read_host_data:{
@@ -58,6 +60,7 @@ void vnr(chanend_t c_frame_in, chanend_t c_frame_out)
         vnr_extract_features(&vnr_feature_state, &feature_patch, feature_patch_data, &X);
 
         float_s32_t inference_output;
+        printf("inference_output: 0x%x, %d\n", inference_output.mant, inference_output.exp);
         vnr_inference(&vnr_ie_state, &inference_output, &feature_patch);
         chan_out_buf_byte(c_frame_out, (uint8_t*)&inference_output, sizeof(float_s32_t));
     }
@@ -68,6 +71,7 @@ void send_frame(chanend_t c_frame_in){
   int32_t *buffer = (int32_t*)&vnr_output;
   while(1){
     chan_in_buf_byte(c_frame_in, (uint8_t *)buffer, sizeof(float_s32_t));
+    printf("inference_output send_frame: 0x%x, %d\n", vnr_output.mant, vnr_output.exp);
     for(int v = 0; v < sizeof(float_s32_t)/4; v++){
       xscope_int(0, buffer[v]);
     }
