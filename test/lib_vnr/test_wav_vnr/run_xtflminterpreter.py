639b32fd0489cec9ac2be0421e4a79ebd93282e7
Shuchita Khare <shuchitak@xmos.com>
2022-05-10 12:20:03 +0100

    Add comment about xmos-ai-tools version needed to get the correct tensor_arena_size_bytes

--- test/lib_vnr/test_wav_vnr/run_xtflminterpreter.py
+++ test/lib_vnr/test_wav_vnr/run_xtflminterpreter.py
@@ -9,7 +9,7 @@ def parse_arguments():
     args = parser.parse_args()
     return args
 
-
+# xmos-ai-tools version 0.1.8.dev93 has the fix for tensor_arena_size to indicate the actual scratch memory that is required by the inference engine
 def run_xtflm_interpreter(xcore_opt_model):
     #xf.convert("model/model_output_0_0_2/model_qaware.tflite", "xcore_opt.tflite", params=None)
     #ie = xtflm.XTFLMInterpreter(model_path="xcore_opt.tflite")
