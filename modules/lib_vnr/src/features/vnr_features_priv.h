f4b6dec404d5e8a6186d7e61ce404532098f6518
Shuchita Khare <shuchitak@xmos.com>
2022-06-09 16:26:03 +0100

    Added documentation to feature extraction defines and API

--- modules/lib_vnr/src/features/vnr_features_priv.h
+++ modules/lib_vnr/src/features/vnr_features_priv.h
@@ -3,6 +3,21 @@
 
 #include "vnr_features_state.h"
 
+/** Exponent of VNR input data. 
+ * NOT USER MODIFIABLE.
+ */
+#define VNR_INPUT_EXP (-31)
+
+/** Extra 2 samples you need to allocate in time domain so that the full spectrum (DC to nyquist) can be stored
+ * after the in-place FFT. NOT USER MODIFIABLE.
+ */  
+#define VNR_FFT_PADDING (2)
+
+/** Exponent of output of the log2 function used in the VNR
+ * @ingroup vnr_features_state
+ */
+#define VNR_LOG2_OUTPUT_EXP (-24)
+   
 // Matching with python names
 void vnr_priv_make_slice(fixed_s32_t *new_slice, const bfp_complex_s32_t *X);
 void vnr_priv_add_new_slice(fixed_s32_t (*feature_buffers)[VNR_MEL_FILTERS], const fixed_s32_t *new_slice);
