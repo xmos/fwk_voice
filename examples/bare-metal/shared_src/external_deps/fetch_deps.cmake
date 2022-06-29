98fbd464fce5d669a971171d46c4ddfc36fea5c0
shuchitak <38428600+shuchitak@users.noreply.github.com>
2022-06-29 11:31:00 +0100

    Disable shallow fetch

--- examples/bare-metal/shared_src/external_deps/fetch_deps.cmake
+++ examples/bare-metal/shared_src/external_deps/fetch_deps.cmake
@@ -5,7 +5,7 @@ FetchContent_Declare(
     xs3_math
     GIT_REPOSITORY https://github.com/xmos/lib_xs3_math.git
     GIT_TAG        b440211755185d97dac8ea72f83f704f8025220f
-    GIT_SHALLOW    TRUE
+    GIT_SHALLOW    FALSE
     SOURCE_DIR     ${CMAKE_BINARY_DIR}/avona_deps/lib_xs3_math
 )
 FetchContent_Populate(xs3_math)
@@ -14,7 +14,7 @@ FetchContent_Declare(
     xscope_fileio
     GIT_REPOSITORY https://github.com/xmos/xscope_fileio.git
     GIT_TAG        86add5101d73d98d4addea9aaeb238072e461b63
-    GIT_SHALLOW    TRUE
+    GIT_SHALLOW    FALSE
     SOURCE_DIR     ${CMAKE_BINARY_DIR}/avona_deps/xscope_fileio
 )
 FetchContent_Populate(xscope_fileio)
