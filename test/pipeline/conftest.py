fd1e54077c4701e6b12b73f6209655905a066dda
Shuchita Khare <shuchitak@xmos.com>
2022-07-26 12:59:23 +0100

    Run all streams in pipeline test

--- test/pipeline/conftest.py
+++ test/pipeline/conftest.py
@@ -61,9 +61,9 @@ def pytest_sessionstart(session):
     else:
         architectures = ["alt_arch"]
 
-    #input_wav_files = [os.path.join(hydra_audio_path, filename) for filename in os.listdir(hydra_audio_path) if (filename.endswith(".wav"))]
+    input_wav_files = [os.path.join(hydra_audio_path, filename) for filename in os.listdir(hydra_audio_path) if (filename.endswith(".wav"))]
     #input_wav_files = [os.path.join(hydra_audio_path, "InHouse_XVF3510v080_v1.2_20190423_Loc3_Noise2_70dB__Take1.wav")]
-    input_wav_files = [os.path.join(hydra_audio_path, "InHouse_XVF3510v080_v1.2_20190423_Loc1_Noise1_65dB_XMOS_DUT1_80dB_Take1.wav")]
+    #input_wav_files = [os.path.join(hydra_audio_path, "InHouse_XVF3510v080_v1.2_20190423_Loc1_Noise1_65dB_XMOS_DUT1_80dB_Take1.wav")]
 
     for input_wav_file in input_wav_files:
         #We sometimes get weird files appearing in dir starting with "._InHouse_X..." so ignore
