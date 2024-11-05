964ff3fd5747db6cfd48678be033e70cda4d54c2
uvvpavel <paveluvarov@xmos.com>
2024-11-05 16:00:55 +0000

    adding a private function back for test compatibility

--- modules/lib_vnr/src/inference/wrapper.h
+++ modules/lib_vnr/src/inference/wrapper.h
@@ -1,9 +1,21 @@
 #pragma once
-#include "vnr_inference_priv.h"
 #include <stdint.h>
+#include "xmath/xmath.h"
+/** Quantisation spec used to quantise the VNR input features and dequantise the VNR output according to the specification for TensorFlow Lite's 8-bit quantization scheme
+ * Quantisation: q = f/input_scale + input_zero_point
+ * Dequantisation: f = output_scale * (q + output_zero_point)
+ */
+typedef struct {
+    float_s32_t input_scale_inv; // Inverse of the input scale
+    float_s32_t input_zero_point;
+    float_s32_t output_scale;
+    float_s32_t output_zero_point;
+}vnr_model_quant_spec_t;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+void vnr_priv_init_quant_spec(vnr_model_quant_spec_t * quant_spec);
     int32_t vnr_init(vnr_model_quant_spec_t * quant_spec);
     int8_t* vnr_get_input();
     int8_t* vnr_get_output();
