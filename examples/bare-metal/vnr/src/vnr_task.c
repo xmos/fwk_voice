a0f5124f88a0d93cd4eed335a4dc5400c2ce30f8
xalbertoisorna <albertoisorna@xmos.com>
2025-11-04 12:10:27 +0000

    Adding back channel.h

--- examples/bare-metal/vnr/src/vnr_task.c
+++ examples/bare-metal/vnr/src/vnr_task.c
@@ -1,6 +1,7 @@
 // Copyright 2022 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
+#include <xcore/channel.h>
 #include <xcore/chanend.h>
 
 #include "vnr.h"
